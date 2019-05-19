521f90f40c4d74b8eee06bbee7ed84bc36933385
Leo Wei <leo.wei@lendi.com.au>
2019-05-20 08:56:52 +1000

    minor

--- src/main/java/spring/ioc/example/Consumer/BarService.java
+++ src/main/java/spring/ioc/example/Consumer/BarService.java
@@ -1,6 +1,5 @@
 package spring.ioc.example.Consumer;
 
-
 public class BarService {
 	public BarService() {}
 	public void start() {
--- src/main/java/spring/ioc/example/Consumer/FooService.java
+++ src/main/java/spring/ioc/example/Consumer/FooService.java
@@ -1,7 +1,5 @@
 package spring.ioc.example.Consumer;
 
-import org.springframework.stereotype.Component;
-
 public class FooService {
 	private BarService barService;
 	public FooService(BarService barService) {
